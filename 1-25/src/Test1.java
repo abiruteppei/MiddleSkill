6b94e801a4f19e69f6972560e52ff556bdd29493
abiruteppei <t.abiru060418@gmail.com>
2023-11-16 23:15:52 +0900

    課題1-25提出_abiru

--- 1-25/src/Test1.java
+++ 1-25/src/Test1.java
@@ -1,35 +1,43 @@
-import java.io.IOException;
-import java.util.logging.ConsoleHandler;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Test1 {
 	public static void main(String[] args) {
-		//改行
+
+		// 改行
+
 		System.out.println("問１");
-		//問題1:下記条件になるようログの出力を行いなさい。
 
-		//条件１　例外でヌルポが発生させヌルポをキャッチした場合のみログ出力がされるプログラムにする事
-		//条件２　"middleskill log"という名前のログのインスタンスを生成する事
-		//条件３　srcディレクトリ内に"middleskill.log"としてログ出力される設定にする事
-		//条件４　ログの内容はコンソールに出ている赤文字と同じ内容のものが表示される事
-		//条件５　格納するログメッセージは"ミドルスキルログが発生しました。"という文言にする事
+		// 問題1: 下記条件になるようログの出力を行いなさい。
 
-		final Logger logger = Logger.getLogger("middleskill log");
-
+		// 条件１ 例外でヌルポが発生させヌルポをキャッチした場合のみログ出力がされるプログラムにする事
 		try {
-			FileHandler fileHandler = new FileHandler("src/middleskill.log");
-			logger.addHandler(fileHandler);
+			throwNullPointerException();
+		} catch (NullPointerException e) {
 
-			ConsoleHandler consoleHandler = new ConsoleHandler();
-			logger.addHandler(consoleHandler);
+			// 条件２ "middleskill log" という名前のログのインスタンスを生成する事
+			Logger logger = Logger.getLogger("middleskill log");
 
-			String str = null;
-			str.toString();
-		} catch (IOException e) {
+			// 条件３ srcディレクトリ内に "middleskill.log" としてログ出力される設定にする事
+			try {
+				FileHandler fileHandler = new FileHandler("src/middleskill.log");
+				logger.addHandler(fileHandler);
+				fileHandler.setFormatter(new SimpleFormatter());
 
-			logger.log(Level.INFO, "ミドルログが発生しました。");
+				// 条件４ ログの内容はコンソールに出ている赤文字と同じ内容のものが表示される事
+				logger.setLevel(Level.ALL);
+
+				// 条件５ 格納するログメッセージは "ミドルスキルログが発生しました。" という文言にする事
+				logger.log(Level.SEVERE, "ミドルスキルログが発生しました。", e);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
 		}
+	}
+
+	private static void throwNullPointerException() {
+		throw new NullPointerException();
 	}
 }
